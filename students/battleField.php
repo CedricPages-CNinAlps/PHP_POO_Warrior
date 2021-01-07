25cda7201621145d1dc6337ace79908e240b02a4
Cedric P <cedric.pages@le-campus-numerique.fr>
2021-01-07 16:59:30 +0100

    Etape 15 à 16 - Done

--- students/battleField.php
+++ students/battleField.php
@@ -6,6 +6,13 @@
 
   class BattleField extends BaseBattleField
   {
-
+      public static function createMyWarrior(){
+          $guerrier = new MarvelWarrior($GLOBALS['warriorID']);
+          $weapon = new Weapon('Hache',100);
+          $weapon->setImageUrl('http://www.no-cookie.com/w/images/thumb/1/18/Hache_de_combat_de_Victo-screen.jpg/340px-Hache_de_combat_de_Victo-screen.jpg');
+          $guerrier->setWeapon($weapon);
+          $guerrier->setImageUrl('https://www.armoria.com/Files/23514/Img/18/Figurine-Guerrier-Viking-brandissant-sa-Hache-en-resine-aspect-bronze-vgr708.jpg');
+          $guerrier->saveNew();
+      }
 
   }
