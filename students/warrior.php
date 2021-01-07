6e750ddf67a85ed5157a544ea48942e3ed8f7a95
Cedric P <cedric.pages@le-campus-numerique.fr>
2021-01-07 14:27:45 +0100

    Etape 3 à 8 - Done

--- students/warrior.php
+++ students/warrior.php
@@ -5,11 +5,64 @@ require_once __DIR__ . "/../base/localWarrior.php";
 
 $GLOBALS['warriorID'] = 'Cedric';
 
-// Définissez vos class de combattants
-class StartrekWarrior {}
-class MarvelWarrior {}
-class PokemonWarrior {}
-
 abstract class Warrior extends LocalWarrior {
+    public $id;
+    public $name;
+    public $speed;
+    public $life;
+    public $shield;
+    public $imageUrl;
+    public $weapon;
+
+    public function __construct($id)
+    {
+        $this->id = $id; // Initialisation de l'ID
+        $this->speed = 30; // Initialisation de la vitesse à 30.
+        $this->life = 100; // Initialisation la vie à 100.
+        $this->shield = 20; // Initialisation du bouclier à 20.
+    }
 
 };
+
+// Définissez vos class de combattants
+class StartrekWarrior extends Warrior {
+    public $mentalPower;
+
+    public function power()
+    {
+        return $this->mentalPower;
+    }
+
+    public function __construct()
+    {
+
+    }
+}
+class MarvelWarrior extends Warrior  {
+    public $superPower;
+
+    public function power()
+    {
+        return $this->superPower;
+    }
+
+    public function __construct()
+    {
+
+    }
+}
+class PokemonWarrior extends Warrior  {
+    public $level;
+
+    public function power()
+    {
+        return $this->level;
+    }
+
+    public function __construct()
+    {
+
+    }
+}
+
+
